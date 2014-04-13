f0eb0db453ae751e3afb00b01d3f557a7165121d
Oliver Delevingne <oliver@delevingne.co.uk>
2014-04-13 22:26:13 +0100

    adding interactive menu, early stages

--- directory.rb
+++ directory.rb
@@ -66,6 +66,30 @@ def print_footer(names)
 puts "Overall, we have #{names.length} great students".center(100)
 end 
 
+
+def interactive_menu
+  loop do 
+    # 1. print the menu and ask the user what to do
+    "1. Input the students"
+    "2. Show the students"
+    "9. Exit"
+  # 1. print the menu and ask the user what to do
+  # 2. read the input and save it into a variable
+  selection = gets.chomp
+  # 3. do what the user has asked
+  case selection
+  when "1"
+    # input the students
+  when "2"
+    # show the students
+  when "9"
+    exit # this will cause the prgram to terminate
+  else 
+    puts "I don't know what you meant, try again"
+  end
+end
+
+
 # nothing happens until we call the methods
 students = input_students # this sets the variable students as the method 'input_students'. This variable then gets used in the following methods.
 print_header
